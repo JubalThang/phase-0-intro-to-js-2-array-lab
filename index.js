37f6b2df3c6954494ace8f988627fe16b65e6bce
jubalthang <jubalthang@gamil.com>
2021-06-19 14:51:36 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) { 
+    let copyOfCasts = cats.slice();
+    copyOfCasts.push(name);
+    return copyOfCasts;
+}
+
+function prependCat(name) {
+    let copyOfCasts = cats.slice();
+    copyOfCasts.unshift(name);
+    return copyOfCasts;
+}
+
+function removeLastCat() {
+    let copyOfCasts = cats.slice();
+    copyOfCasts.pop();
+    return copyOfCasts;
+}
+
+function removeFirstCat() {
+    let copyOfCasts = cats.slice();
+    copyOfCasts.shift();
+    return copyOfCasts;
+}
